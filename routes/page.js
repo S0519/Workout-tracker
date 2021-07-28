daad5f5040986e63c41c145e863b89acc2c9a4d5
Sara Kanga <sarakanga600@gmail.com>
2021-07-27 23:59:51 -0500

    add routes files

---
+++ routes/page.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+
+const router = express.Router();
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+export default router;
