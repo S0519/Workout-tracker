6dc995fc1a5583bdfe34d6e63b76d4254b4f3bad
Sara Kanga <sarakanga600@gmail.com>
2021-07-28 14:43:18 -0500

    resolve path&dirname module import

--- routes/page.js
+++ routes/page.js
@@ -1,17 +1,20 @@
 import express from "express";
-import * as path, { __dirname } from 'path';
+import path from 'path';
 
+const __dirname = path.resolve();
+console.log('test11111111');
+console.log(__dirname);
 const router = express.Router();
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
 });
 
 router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
+    res.sendFile(path.join(__dirname, "./public/stats.html"));
 });
 
 export default router;
